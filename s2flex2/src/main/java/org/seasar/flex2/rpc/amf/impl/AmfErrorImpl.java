877a467c8aa906cebfd7b6cd90ee667b7ed0073d
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-03-12 16:28:39 +0000

    1.package構成変更 2.AMF3の実装をコミット
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@99 c2bc002a-290a-0410-b2b5-f81595976b5c

---
+++ s2flex2/src/main/java/org/seasar/flex2/rpc/amf/impl/AmfErrorImpl.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.flex2.rpc.amf.impl;
+
+import org.seasar.flex2.rpc.amf.AmfError;
+
+public class AmfErrorImpl implements AmfError {
+
+	private String code = "SERVER.PROCESSING";
+	private String type;
+	private String level = "error";
+	private String details;
+	private String description;
+	
+	public AmfErrorImpl(Throwable t) {
+		type = t.getClass().getName();
+		details = getStackTraceString(t);
+		description = t.getMessage();
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	private static String getStackTraceString(Throwable t) {
+		StackTraceElement[] elements = t.getStackTrace();
+		StringBuffer buf = new StringBuffer(t.toString());
+		buf.append('\n');
+		for (int i = 0; i < elements.length; ++i) {
+			buf.append(elements[i].toString());
+			buf.append('\n');
+		}
+		return buf.toString();
+	}
+}
---
+++ s2flex2/src/main/java/org/seasar/flex2/rpc/gateway/invoker/impl/S2ContainerInvoker.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.flex2.rpc.gateway.invoker.impl;
+
+import org.seasar.flex2.rpc.gateway.invoker.ServiceInvoker;
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.exception.InvocationTargetRuntimeException;
+import org.seasar.framework.util.ClassUtil;
+
+public class S2ContainerInvoker implements ServiceInvoker {
+    
+    private S2Container container;
+    
+	public S2ContainerInvoker() {
+	}
+    
+    public void setContainer(S2Container container) {
+        this.container = container;
+    }
+
+	public boolean supports(String serviceName, String methodName, Object[] args) {
+		S2Container root = container.getRoot();
+		if (root.hasComponentDef(serviceName)) {
+			return true;
+		}
+		try {
+			Class clazz = ClassUtil.forName(serviceName);
+			if (root.hasComponentDef(clazz)) {
+				return true;
+			}
+		} catch (Throwable ignore) {
+		}
+		return false;
+	}
+
+	public Object invoke(String serviceName, String methodName, Object[] args)
+			throws Throwable {
+
+		S2Container root = container.getRoot();
+		Object component = null;
+		Class clazz = null;
+		if (root.hasComponentDef(serviceName)) {
+			component = root.getComponent(serviceName);
+		} else {
+			clazz = ClassUtil.forName(serviceName);
+			component = root.getComponent(clazz);
+		}
+		BeanDesc beanDesc = BeanDescFactory.getBeanDesc(component.getClass());
+        try {
+            return beanDesc.invoke(component, methodName, args);
+        } catch (InvocationTargetRuntimeException e) {
+            throw e.getCause();
+        }
+	}
+}
