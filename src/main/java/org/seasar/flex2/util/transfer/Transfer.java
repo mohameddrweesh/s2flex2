f77b22bad8900cad7deaaa90b07baf77c080d28e
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-04-11 07:14:18 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk/s2flex2@248 c2bc002a-290a-0410-b2b5-f81595976b5c

--- src/main/java/org/seasar/flex2/util/transfer/Transfer.java
+++ src/main/java/org/seasar/flex2/util/transfer/Transfer.java
@@ -4,8 +4,7 @@ import org.seasar.flex2.util.transfer.storage.Storage;
 
 public interface Transfer {
 
-    public abstract void importTo(Storage storage, Object target);
+    void importToData(Storage storage, Object target);
 
-    public abstract void exportTo(Storage storage, Object target);
-
+    void exportToStorage(Object target, Storage storage);
 }
