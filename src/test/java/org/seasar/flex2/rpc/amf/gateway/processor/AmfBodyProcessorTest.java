64ad4335ec526817300a6eb3b856e2c53e17a8b6
nod <nod@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-05-11 09:36:29 +0000

    AmfBodyProcessorImpl testCase
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk/s2flex2@317 c2bc002a-290a-0410-b2b5-f81595976b5c

---
+++ src/test/java/org/seasar/flex2/rpc/amf/gateway/processor/AmfBodyProcessorTest.java
@@ -0,0 +1,38 @@
+package org.seasar.flex2.rpc.amf.gateway.processor;
+
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.flex2.rpc.amf.data.factory.AmfBodyFactory;
+import org.seasar.flex2.rpc.amf.data.factory.AmfErrorFactory;
+import org.seasar.flex2.rpc.amf.data.factory.AmfMessageFactory;
+import org.seasar.flex2.rpc.amf.gateway.processor.impl.AmfBodyProcessorImpl;
+import org.seasar.framework.container.S2Container;
+
+public class AmfBodyProcessorTest extends S2TestCase {
+
+    private static String PATH = "Amf3ProcessorTest.dicon";
+
+    public void testCreateProcessor() throws Exception {
+        S2Container container = getContainer();
+        Object processor = container.getComponent(AmfBodyProcessorImpl.class);
+        
+        assertTrue("1", processor instanceof AmfBodyProcessorImpl);
+
+        AmfBodyProcessorImpl processorImpl = (AmfBodyProcessorImpl) processor;
+        assertTrue(
+                "2",
+                processorImpl.getBodyFactory() instanceof AmfBodyFactory);
+        assertTrue(
+                "3",
+                processorImpl.getErrorFactory() instanceof AmfErrorFactory);
+        assertTrue(
+                "4",
+                processorImpl.getMessageFactory() instanceof AmfMessageFactory);
+        
+    }
+    protected void setUp() throws Exception {
+        include(PATH);
+    }
+
+    protected void tearDown() throws Exception {
+    }
+}
