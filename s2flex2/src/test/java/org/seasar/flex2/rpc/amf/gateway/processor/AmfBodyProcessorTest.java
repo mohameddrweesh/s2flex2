cd72431756e925a890a5bbe528cd715b164a477d
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-05-11 09:49:04 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@320 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2/src/test/java/org/seasar/flex2/rpc/amf/gateway/processor/AmfBodyProcessorTest.java
+++ s2flex2/src/test/java/org/seasar/flex2/rpc/amf/gateway/processor/AmfBodyProcessorTest.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.seasar.flex2.rpc.amf.gateway.processor;
 
 import org.seasar.extension.unit.S2TestCase;
-import org.seasar.flex2.rpc.amf.data.factory.AmfBodyFactory;
-import org.seasar.flex2.rpc.amf.data.factory.AmfErrorFactory;
 import org.seasar.flex2.rpc.amf.data.factory.AmfMessageFactory;
+import org.seasar.flex2.rpc.amf.data.factory.impl.Amf3BodyFactoryImpl;
+import org.seasar.flex2.rpc.amf.data.factory.impl.AmfErrorFactoryImpl;
 import org.seasar.flex2.rpc.amf.gateway.processor.impl.AmfBodyProcessorImpl;
 import org.seasar.framework.container.S2Container;
 
@@ -13,24 +28,19 @@ public class AmfBodyProcessorTest extends S2TestCase {
 
     public void testCreateProcessor() throws Exception {
         S2Container container = getContainer();
-        Object processor = container.getComponent(AmfBodyProcessorImpl.class);
-        
-        assertTrue("1", processor instanceof AmfBodyProcessorImpl);
+        Object processor = container.getComponent(AmfBodyProcessor.class);
+        assertTrue("1", processor instanceof AmfBodyProcessor);
 
         AmfBodyProcessorImpl processorImpl = (AmfBodyProcessorImpl) processor;
-        assertTrue(
-                "2",
-                processorImpl.getBodyFactory() instanceof AmfBodyFactory);
-        /*
-        assertTrue(
-                "3",
-                processorImpl.getErrorFactory() instanceof AmfErrorFactory);
-         */
-        assertTrue(
-                "4",
+        assertTrue("2",
+                processorImpl.getBodyFactory() instanceof Amf3BodyFactoryImpl);
+        assertTrue("3",
+                processorImpl.getErrorFactory() instanceof AmfErrorFactoryImpl);
+        assertTrue("4",
                 processorImpl.getMessageFactory() instanceof AmfMessageFactory);
-        
+
     }
+
     protected void setUp() throws Exception {
         include(PATH);
     }
