8155f9d7c0a9f72fc6715c845fe25d8d1d4cf1e1
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-05-17 09:49:10 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@383 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2/src/test/java/org/seasar/flex2/util/data/transfer/TestClass.java
+++ s2flex2/src/test/java/org/seasar/flex2/util/data/transfer/TestClass.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.flex2.util.transfer;
+package org.seasar.flex2.util.data.transfer;
 
 public class TestClass {
     private String strData;
