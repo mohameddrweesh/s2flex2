81091d645864f24324d79588153f6689c2eaa085
nod <nod@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-05-22 14:42:15 +0000

    restore
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@449 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2-tiger/src/main/java/org/seasar/flex2/rpc/amf/gateway/service/annotation/AmfRemotingService.java
+++ s2flex2-tiger/src/main/java/org/seasar/flex2/rpc/amf/gateway/service/annotation/AmfRemotingService.java
@@ -15,5 +15,12 @@
  */
 package org.seasar.flex2.rpc.amf.gateway.service.annotation;
 
-public interface AmfRemotingService {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.TYPE })
+public @interface AmfRemotingService {
 }
