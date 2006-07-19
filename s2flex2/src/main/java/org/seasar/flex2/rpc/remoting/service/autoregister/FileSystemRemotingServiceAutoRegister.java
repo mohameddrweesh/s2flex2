cdddab63bf42352c22d99e3193f6ccd182c0af6f
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-07-19 02:41:28 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@708 c2bc002a-290a-0410-b2b5-f81595976b5c

---
+++ s2flex2/src/main/java/org/seasar/flex2/rpc/remoting/service/autoregister/FileSystemRemotingServiceAutoRegister.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.flex2.rpc.remoting.service.autoregister;
+
+import org.seasar.flex2.rpc.remoting.service.RemotingServiceLocator;
+import org.seasar.flex2.rpc.remoting.service.RemotingServiceRepository;
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.autoregister.AbstractComponentTargetAutoRegister;
+
+public class FileSystemRemotingServiceAutoRegister extends
+        AbstractComponentTargetAutoRegister {
+
+    private RemotingServiceLocator locator;
+
+    private RemotingServiceRepository repository;
+
+    public void setLocator(RemotingServiceLocator locator) {
+        this.locator = locator;
+    }
+
+    public void setRepository(RemotingServiceRepository repository) {
+        this.repository = repository;
+    }
+
+    protected void register( final ComponentDef cd) {
+        if (locator.isSupportService(cd.getComponentName())) {
+            repository.addService(cd.getComponentName(), getContainer()
+                    .getComponentDef(cd.getComponentName()));
+        }
+    }
+}
