9e8df0d184d82bed0a0c86afdae1385fc9bf9e06
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-09-06 06:31:05 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@947 c2bc002a-290a-0410-b2b5-f81595976b5c

---
+++ s2flex2/src/test/java/org/seasar/flex2/rpc/remoting/message/processor/MessageBodyProcessorTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.flex2.rpc.remoting.message.data.processor;
+
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.flex2.rpc.remoting.message.data.factory.MessageFactory;
+import org.seasar.flex2.rpc.remoting.message.data.factory.impl.FaultFactoryImpl;
+import org.seasar.flex2.rpc.remoting.message.data.factory.impl.MessageBodyFactoryImpl;
+import org.seasar.flex2.rpc.remoting.message.data.processor.impl.MessageBodyProcessorImpl;
+import org.seasar.framework.container.S2Container;
+
+public class MessageBodyProcessorTest extends S2TestCase {
+
+    private static String PATH = "remoting_amf3.dicon";
+
+    public void testCreateProcessor() throws Exception {
+        S2Container container = getContainer();
+        Object processor = container.getComponent(MessageBodyProcessor.class);
+        assertTrue("1", processor instanceof MessageBodyProcessor);
+
+        MessageBodyProcessorImpl processorImpl = (MessageBodyProcessorImpl) processor;
+        assertTrue(
+                "2",
+                processorImpl.getBodyFactory() instanceof MessageBodyFactoryImpl);
+        assertTrue("3",
+                processorImpl.getFaultFactory() instanceof FaultFactoryImpl);
+        assertTrue("4",
+                processorImpl.getMessageFactory() instanceof MessageFactory);
+
+    }
+
+    protected void setUp() throws Exception {
+        include(PATH);
+    }
+
+    protected void tearDown() throws Exception {
+    }
+}
