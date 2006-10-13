fd507c163040f185f71103a29d2c179e748c7e75
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2006-10-13 01:24:55 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@1025 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2/src/main/java/org/seasar/flex2/util/converter/impl/AbstractArrayConverterImpl.java
+++ s2flex2/src/main/java/org/seasar/flex2/util/converter/impl/AbstractArrayConverterImpl.java
@@ -19,7 +19,7 @@ import java.lang.reflect.Array;
 import java.util.Map;
 
 public abstract class AbstractArrayConverterImpl extends
-        AbstractMapConverterImpl {
+        AbstractBeanConverterImpl {
 
     protected static final Object convertToArray(final Object source,
             final Class distClass) {
