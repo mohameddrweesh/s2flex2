60cbb52bd81150697ae7eca15c5aa29ebce9e311
nod <nod@c2bc002a-290a-0410-b2b5-f81595976b5c>
2007-02-12 09:39:44 +0000

    for s2ana-releated(AMF0)improved.
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@1189 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2/src/main/java/org/seasar/flex2/rpc/remoting/message/RemotingMessageConstants.java
+++ s2flex2/src/main/java/org/seasar/flex2/rpc/remoting/message/RemotingMessageConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2006 the Seasar Foundation and the Others.
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,15 +15,27 @@
  */
 package org.seasar.flex2.rpc.remoting.message;
 
+/**
+ * <h4>RemotingMessageConstants</h4>
+ * RemotingMessageで利用される定数を定義するインターフェース
+ * @author e1.arkw
+ * @author nod
+ *
+ */
 public interface RemotingMessageConstants {
 
-    String APPEND_TO_GATEWAYURL = "AppendToGatewayUrl";
+    static final String APPEND_TO_GATEWAYURL = "AppendToGatewayUrl";
     
-    String CREDENTIALS_PASSWORD = "credentialsPassword";
+    static final String CREDENTIALS_PASSWORD = "credentialsPassword";
 
-    String CREDENTIALS_USERNAME = "credentialsUsername";
+    static final String CREDENTIALS_USERNAME = "credentialsUsername";
 
-    String REMOTE_CREDENTIALS_PASSWORD = "remoteCredentialsPassword";
+    static final String REMOTE_CREDENTIALS_PASSWORD = "remoteCredentialsPassword";
 
-    String REMOTE_CREDENTIALS_USERNAME = "remoteCredentialsUsername";
+    static final String REMOTE_CREDENTIALS_USERNAME = "remoteCredentialsUsername";
+    
+    /** 認証用のヘッダ名称を示す定数 */
+    static final String HEADER_NAME_CREDENTINALS= "Credentials";
+    static final String USERID="userid";
+    static final String PASSWORD="password";
 }
