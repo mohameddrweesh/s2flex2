981f19b04ecb65d557ac36293effd05ca1e40676
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2007-03-19 06:06:07 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@1285 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2/src/main/java/org/seasar/flex2/util/type/BooleanUtil.java
+++ s2flex2/src/main/java/org/seasar/flex2/util/type/BooleanUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2006 the Seasar Foundation and the Others.
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.flex2.util;
+package org.seasar.flex2.util.type;
 
 public class BooleanUtil {
     public static boolean toBoolean(final String name) {
