e3c638e001589c058eebd30389567b02d97e7667
e1arkw <e1arkw@c2bc002a-290a-0410-b2b5-f81595976b5c>
2007-03-19 06:32:06 +0000

    gatewayVersionをparameterで変更できるようにした。
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@1287 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2/src/main/java/org/seasar/flex2/rpc/remoting/RemotingGateway.java
+++ s2flex2/src/main/java/org/seasar/flex2/rpc/remoting/RemotingGateway.java
@@ -26,31 +26,32 @@ import org.seasar.flex2.rpc.remoting.processor.RemotingMessageProcessor;
 import org.seasar.flex2.util.type.BooleanUtil;
 import org.seasar.framework.container.S2Container;
 import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.framework.log.Logger;
 
 public class RemotingGateway extends HttpServlet {
 
-    public static final String VERSION = "1.0.2";
+    private static final long serialVersionUID = -5142496558861670626L;
 
-    /**
-     * 
-     */
+    protected static final String version = "1.0.2";
+
+    protected static final Logger logger = Logger
+            .getLogger(RemotingGateway.class);
+
+    private static final String GATEWAY_VERSION = "gatewayVersion";
+
     private static final String PARAMETER_SHOW_GET_RESPONSE = "showGetResponse";
 
-    /**
-     * 
-     */
     private static final String PARAMETER_USE_SESSION = "useSession";
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 4841210303706096594L;
+    protected RemotingMessageProcessor processor;
+
+    private String gatewayVersion;
+
+    private String gatewayVersionMessage;
 
     private boolean isShowGetResponse;
 
     private boolean isUseSession;
-
-    protected RemotingMessageProcessor processor;
 
     public void doGet(final HttpServletRequest request,
             final HttpServletResponse response) throws IOException,
@@ -71,13 +72,10 @@ public class RemotingGateway extends HttpServlet {
                 .getContainer().getRoot();
         processor = (RemotingMessageProcessor) container
                 .getComponent(RemotingMessageProcessor.class);
-        isShowGetResponse = getInitBooleanParameter(PARAMETER_SHOW_GET_RESPONSE);
-        isUseSession = getInitBooleanParameter(PARAMETER_USE_SESSION);
+        initGatewayVersionMessage();
+        logger.info(gatewayVersionMessage);
     }
 
-    /**
-     * @param request
-     */
     private final void decisionSessionMode(final HttpServletRequest request) {
         if (isUseSession) {
             if (request.isRequestedSessionIdValid()) {
@@ -86,19 +84,27 @@ public class RemotingGateway extends HttpServlet {
         }
     }
 
-    private final boolean getInitBooleanParameter(final String name) {
+    private final String getGatewayVersionParameter(final String name) {
+        final String value = this.getInitParameter(name);
+        return (value != null ? value : version) + " ";
+    }
+
+    private final boolean getInitParameterAsBoolean(final String name) {
         return BooleanUtil.toBoolean(this.getInitParameter(name));
     }
 
-    /**
-     * @param response
-     * @throws IOException
-     */
+    private final void initGatewayVersionMessage() {
+        isShowGetResponse = getInitParameterAsBoolean(PARAMETER_SHOW_GET_RESPONSE);
+        isUseSession = getInitParameterAsBoolean(PARAMETER_USE_SESSION);
+        gatewayVersion = getGatewayVersionParameter(GATEWAY_VERSION);
+        gatewayVersionMessage = "RemotingGateway " + gatewayVersion
+                + "is running ...";
+    }
+
     private final void showGetResponse(final HttpServletResponse response)
             throws IOException {
         if (isShowGetResponse) {
-            response.getWriter().write(
-                    "RemotingGateway " + VERSION + " is running ...");
+            response.getWriter().write(gatewayVersionMessage);
         }
     }
 }
