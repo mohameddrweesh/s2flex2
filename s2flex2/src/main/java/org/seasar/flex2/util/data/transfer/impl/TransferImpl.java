aa5a4316d8ad224167899843a5d6386b9d9257bd
nod <nod@c2bc002a-290a-0410-b2b5-f81595976b5c>
2008-03-06 05:02:33 +0000

    コメントの追加
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/trunk@1477 c2bc002a-290a-0410-b2b5-f81595976b5c

--- s2flex2/src/main/java/org/seasar/flex2/util/data/transfer/impl/TransferImpl.java
+++ s2flex2/src/main/java/org/seasar/flex2/util/data/transfer/impl/TransferImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2006 the Seasar Foundation and the Others.
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,17 +23,44 @@ import org.seasar.framework.beans.BeanDesc;
 import org.seasar.framework.beans.PropertyDesc;
 import org.seasar.framework.beans.factory.BeanDescFactory;
 
+/**
+ *  {@link Tranfer}Tranferの実装クラス
+ * @author e1.arkw
+ * @author nod
+ *
+ */
 public class TransferImpl implements Transfer {
-
+    /** アノテーションハンドラ. 実行環境によって定数アノテーションかjava5.0のアノテーションのいづれかのハンドラがセットされます。*/
     private static final AnnotationHandler annotationHandler = AnnotationHandlerFactory
             .getAnnotationHandler();
-
+    /**
+     * 
+     * @param storage 保存対象のStorage
+     * @param type ストレージのタイプ.{@link Storage}によって定義されている値
+     * @return 転送対象のときはtrue,それ以外はfalse.
+     */
     private static final boolean isTransferTarget(final Storage storage,
             final String type) {
         return ((type != null) && storage.getName().equalsIgnoreCase(type));
     }
 
+    /**
+     * targetになるObjectのreadMethodにExportアノテーションが存在するとき
+     * ComponentsをStorageに保存します。
+     *  
+     *  @param target Storageに保存するオブジェクト
+     *  @param storage 保存先のストレージ
+     */
     public void exportToStorage(final Object target, final Storage storage) {
+        //ExportアノテーションがgetMethodにあるときにはStorageにComponentを保存する。
+        /*
+         * @Export(storage=StorageType.SESSION)
+         * public AddDto getAddDto() {
+         *       return addDto;
+         * }
+         * 上記のように書かれてときには、Sessionに対してaddDtoが保存される。
+         */
+        
         final BeanDesc beanDesc = BeanDescFactory
                 .getBeanDesc(target.getClass());
 
@@ -49,8 +76,24 @@ public class TransferImpl implements Transfer {
             }
         }
     }
-
+    /**
+     * 指定されたObjectのwriteMethodにImportアノテーションが存在しているとき
+     * StorageからComponentにセットします。
+     * 
+     * @param storage 保存されているストレージ
+     * @param target セットする対象のオブジェクト
+     */
     public void importToComponent(final Storage storage, final Object target) {
+        /*
+         * @Import(storage=StorageType.SESSION)
+         * public AddDto setAddDto(AddDto addDto) {
+         *       this.addDto = addDto;
+         * }
+         * 上記のように書かれてときには、Sessionデータから取得したObjectがセットされる。
+         */
+        
+        //ImportアノテーションがコンポーネントのsetMethodについているときに、
+        //Storageから取得したComponentをセットする。
         final BeanDesc beanDesc = BeanDescFactory
                 .getBeanDesc(target.getClass());
         for (int i = 0; i < beanDesc.getPropertyDescSize(); ++i) {
