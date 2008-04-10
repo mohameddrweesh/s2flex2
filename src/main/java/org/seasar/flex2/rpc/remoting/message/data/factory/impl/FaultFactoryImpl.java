9a9cb1f4a15ef141052fe2e81357865e24de180f
nod <nod@c2bc002a-290a-0410-b2b5-f81595976b5c>
2008-04-10 07:37:53 +0000

    rootCause対応
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/branches/s2flex2-1.0.x@1485 c2bc002a-290a-0410-b2b5-f81595976b5c

--- src/main/java/org/seasar/flex2/rpc/remoting/message/data/factory/impl/FaultFactoryImpl.java
+++ src/main/java/org/seasar/flex2/rpc/remoting/message/data/factory/impl/FaultFactoryImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2006 the Seasar Foundation and the Others.
+ * Copyright 2004-2008 the Seasar Foundation and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,19 @@ import org.seasar.flex2.rpc.remoting.message.data.Fault;
 import org.seasar.flex2.rpc.remoting.message.data.factory.FaultFactory;
 import org.seasar.framework.container.S2Container;
 
+/**
+ * {@link Fault}のFactory実装クラスです
+ * @author e1.arkw
+ * @author nod
+ *
+ */
 public class FaultFactoryImpl implements FaultFactory {
 
+    /**
+     * StackTraceを文字列にして返します
+     * @param t StackTraceを出力する対象
+     * @return　StackTraceの内容を文字列化したもの
+     */
     private static final String getStackTraceString(final Throwable t) {
         final StackTraceElement[] elements = t.getStackTrace();
         final StringBuffer buf = new StringBuffer(t.toString());
@@ -32,23 +43,43 @@ public class FaultFactoryImpl implements FaultFactory {
         return buf.toString();
     }
 
+    /**
+     * @exclude
+     */
     private S2Container container;
 
+    /**
+     * 　{@link Fault}を生成します
+     * @param type タイプ
+     * @param details　詳細メッセージ
+     * @param description　エラー情報
+     * @param rootCause　発生元の例外
+     * @return　Faultのインスタンス
+     */
     public Fault createFault(final String type, final String details,
-            final String description) {
+            final String description,final Throwable rootCause) {
         final Fault fault = (Fault) container.getComponent(Fault.class);
         fault.setType(type);
-        fault.setDetails(details);
-        fault.setDescription(description);
-
+        fault.setFaultDetail(details);
+        fault.setFaultString(description);
+        fault.setRootCause(rootCause);
         return fault;
     }
-
+    
+    /**
+     * [{@link Fault}を生成して返します
+     * @param throwable 発生した例外
+     * @return 例外をもとに生成したFault クラス
+     */
     public Fault createFault(final Throwable throwable) {
         return createFault(throwable.getClass().getName(),
-                getStackTraceString(throwable), throwable.getMessage());
+                getStackTraceString(throwable), throwable.getMessage(),throwable);
     }
 
+
+    /**
+     * @exclude
+     */
     public void setContainer(final S2Container container) {
         this.container = container;
     }
