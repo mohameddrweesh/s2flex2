284131d0ddb92dde9e8d37bf439caaba66765d13
nod <nod@c2bc002a-290a-0410-b2b5-f81595976b5c>
2008-04-29 11:44:43 +0000

    version is now 1.0.3
    git-svn-id: https://www.seasar.org/svn/sandbox/s2flex2/branches/s2flex2-1.0.x@1490 c2bc002a-290a-0410-b2b5-f81595976b5c

--- src/main/java/org/seasar/flex2/rpc/remoting/RemotingGateway.java
+++ src/main/java/org/seasar/flex2/rpc/remoting/RemotingGateway.java
@@ -29,7 +29,7 @@ import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
 
 public class RemotingGateway extends HttpServlet {
 
-    public static final String VERSION = "1.0.1-snapshot";
+    public static final String VERSION = "1.0.3";
 
     /**
      * 
